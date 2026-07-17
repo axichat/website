8298c94f23629a51812249e7ac48f16f7aadf6e0
eliot <eliot@axichat.com>
2026-07-17 14:04:33 -0700

    Improve metadata.

--- vite.config.ts
+++ vite.config.ts
@@ -14,6 +14,7 @@ export default defineConfig({
     rollupOptions: {
       input: {
         main: resolve(projectRoot, "index.html"),
+        notFound: resolve(projectRoot, "404.html"),
         register: resolve(projectRoot, "register/index.html"),
       },
     },
